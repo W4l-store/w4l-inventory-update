a6a481cebadda3f91e7ef11e49f203b39e6ef9be
V-Andriy <111814295+V-Andriy@users.noreply.github.com>
2024-07-18 20:18:49 -0400

    add double roles map

--- utils/update_resources.py
+++ utils/update_resources.py
@@ -5,19 +5,20 @@ from google.oauth2.service_account import Credentials
 import logging
 from .helpers import a_ph
 import os
+import json
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def update_resources():
-    update_amz_NA_mapping()
+    update_from_google_sheet()
+
+def update_from_google_sheet():
+    amz_NA_mapping_worksheet_name = "final_NA_mapping"
+    update_double_roles_map_worksheet_name = "double_roles_map"
 
 
-
-def update_amz_NA_mapping():
-    logging.info('Starting update_amz_NA_mapping function')
-    folder_path = a_ph('/resources/amazon/BS_SKU_mapping/NA_mapping')
-
+ 
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
     creds = Credentials.from_service_account_file(a_ph("credentials/sheets_api_cred.json"), scopes=scopes)
     client = gspread.authorize(creds)
@@ -26,17 +27,38 @@ def update_amz_NA_mapping():
     workbook = client.open_by_key(sheet_id)
 
     worksheet_list = map(lambda x: x.title, workbook.worksheets())
-    source_worksheet_name = "final_NA_mapping"
     
-    if source_worksheet_name not in worksheet_list:
-        logging.error(f"Worksheet {source_worksheet_name} not found in the google sheet")
-        raise ValueError(f"Worksheet {source_worksheet_name} not found in the google sheet")
+    if amz_NA_mapping_worksheet_name not in worksheet_list:
+        logging.error(f"Worksheet {amz_NA_mapping_worksheet_name} not found in the google sheet")
+        raise ValueError(f"Worksheet {amz_NA_mapping_worksheet_name} not found in the google sheet")
     # get the worksheet to df 
-    source_worksheet = workbook.worksheet(source_worksheet_name)
-    records = source_worksheet.get_all_records()
-    source_df = pd.DataFrame(records, dtype=str)
-    source_df.to_csv(os.path.join(folder_path, 'amz_NA_mapping.csv'), index=False)
-    logging.info('Finished update_amz_NA_mapping function')
+    amz_NA_mapping_df = pd.DataFrame(workbook.worksheet(amz_NA_mapping_worksheet_name).get_all_records())
+    
+    update_amz_NA_mapping(amz_NA_mapping_df)
+
+    if update_double_roles_map_worksheet_name not in worksheet_list:
+        logging.error(f"Worksheet {update_double_roles_map_worksheet_name} not found in the google sheet")
+        raise ValueError(f"Worksheet {update_double_roles_map_worksheet_name} not found in the google sheet")
+
+    update_double_roles_map_df = pd.DataFrame(workbook.worksheet(update_double_roles_map_worksheet_name).get_all_records())
+    update_double_roles_map(update_double_roles_map_df)
+
+
+
+
+
+def update_amz_NA_mapping(amz_NA_mapping_df: pd.DataFrame):
+   
+    amz_NA_mapping_df.to_csv(os.path.join(a_ph('/resources/amazon/BS_SKU_mapping/NA_mapping'), 'amz_NA_mapping.csv'), index=False)
+    logger.info("Updated NA mapping")
+
+def update_double_roles_map(update_double_roles_map_df):
+    #resources/blue_sistem/double_roles_map.json
+    double_roles_map = dict(zip(update_double_roles_map_df['NAME'].str.strip(), update_double_roles_map_df['delimiter'].astype(int)))
+    with open(a_ph('/resources/blue_sistem/double_roles_map.json'), 'w') as f:
+        json.dump(double_roles_map, f)
+    logger.info("Updated double roles map")
+
 
 # test run 
 def test():
